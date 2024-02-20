0dcd180046d141cf23817bc8ac1a7408f4b33f3a
stuti87 <stuti.karol@gmail.com>
2024-02-20 06:47:41 +0530

    loan project first checking

---
+++ src/main/java/com/springbc/first/loancheckbcproj/LoanCheckbcprojApplication.java
@@ -0,0 +1,13 @@
+package com.springbc.first.loancheckbcproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanCheckbcprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanCheckbcprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbc/first/loancheckbcproj/LoanCheckbcprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbc.first.loancheckbcproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCheckbcprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
